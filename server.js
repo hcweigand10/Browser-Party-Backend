1f75f821f408fee6f18794817fed3078bcb7d2c0
hcweigand10 <henryweigand10@gmail.com>
2023-02-24 15:39:57 -0800

    better than cors

--- server.js
+++ server.js
@@ -11,12 +11,25 @@ const app = express();
 // app.use(cors());
 
 // DEPLOYED
-app.use(cors({
-  origin:"https://browserparty.netlify.app" // change
-}))
+// app.use(cors({
+//   origin:"https://browserparty.netlify.app" // change
+// }))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:3000', 'https://loranscruggs.netlify.app', 'http://loranscruggs.com', 'https://loranscruggs.com' ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
+
 app.use(routes);
 
 db.once('open', () => {
